54ef4bc8d6516817c18daaa452b548741809a1fe
the390r <solopoff13@yandex.ru>
2019-02-20 08:58:00 +0300

    Upload

---
+++ Project1.dpr
@@ -0,0 +1,15 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+ Writeln('Hello, World from Delphi!');
+  Writeln('This is the console application');
+  Writeln;
+  Writeln('Press <Enter> to quit.');
+  Readln; 
+
+end.
